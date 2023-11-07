73d2c7fcf84c75d785862a4f5b3b4e77809f6f67
Mohan Kumar <mohankumar.parise523@gmail.com>
2023-11-07 16:40:30 +0530

    init

---
+++ javascript/MyFirstFunction.js
@@ -0,0 +1,25 @@
+function intro() {
+  console.log("hello world");
+}
+function add(num1, num2) {
+  return num1 + num2;
+}
+function sub(num1, num2) {
+  return num1 - num2;
+}
+function mult(num1, num2) {
+  return num1 * num2;
+}
+function division(num1, num2) {
+  return num1 / num2;
+}
+
+function Calculator(num1, num2, operation) {
+  let val = operation(num1, num2);
+  return val;
+}
+
+intro();
+
+let i = Calculator(4, 5, add);
+console.log(i);
